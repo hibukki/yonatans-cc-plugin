a1929b87b6fa51537a3ed10e759a9b4c677d4db3
Yonatan Cale <myselfandfredy@gmail.com>
2026-01-23 21:59:56 -0800

    webfetch: don't wait for user approval

--- plugins/quick-review/hooks/on-web-fetch.sh
+++ plugins/quick-review/hooks/on-web-fetch.sh
@@ -7,7 +7,7 @@ cat <<'EOF'
 {
   "hookSpecificOutput": {
     "hookEventName": "PostToolUse",
-    "additionalContext": "Tip: If WebFetch is filtering out info you want, you can download the entire webpage and launch a sub task to get info from that downloaded file."
+    "additionalContext": "Tip: If WebFetch is filtering out info you want, you can download the entire webpage and launch a sub task to get info from that downloaded file. If that would be helpful, do it without asking the user for permission (unless the user indicated another preference)."
   }
 }
 EOF
