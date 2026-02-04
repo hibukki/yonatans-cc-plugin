3d423dc2f692c41b1fcb1165dfba5f3deb191b72
Yonatan Cale <myselfandfredy@gmail.com>
2026-02-03 18:34:18 -0800

    test-post-async: disable to isolate on-commit-async debugging

--- plugins/quick-review/hooks/test-post-async.sh
+++ plugins/quick-review/hooks/test-post-async.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Simple test for PostToolUse async hook delivery
-# Returns immediately with a debug message
+# This test WORKED - it successfully delivered messages to Claude
+# Disabled to avoid interference while debugging on-commit-async.sh
 
-sleep 2
-echo '{"systemMessage": "DEBUG: PostToolUse async hook completed"}'
+exit 0  # DISABLED - uncomment below to re-enable
+# sleep 2
+# echo '{"systemMessage": "DEBUG: PostToolUse async hook completed"}'
