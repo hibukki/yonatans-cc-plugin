94b8e226c0517e96e850250aea823964efe487c7
Yonatan Cale <myselfandfredy@gmail.com>
2026-02-03 18:27:39 -0800

    +test-post-async.sh: test PostToolUse async delivery

---
+++ plugins/quick-review/hooks/test-post-async.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Simple test for PostToolUse async hook delivery
+# Returns immediately with a debug message
+
+sleep 2
+echo '{"systemMessage": "DEBUG: PostToolUse async hook completed"}'
